82db6b8abe6ad5a87392e4e0490c90693d761d52
vetal11 <vetalblack@list.ru>
2018-10-04 20:17:13 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let num = number;
+  let b = base;
+  for(let i = 2; i <= b; i++){
+    let a = 0;
+      while(b % i == 0){
+        a++;
+        b = Math.floor(b/i);
+      }
+    let x = 0;
+    let  num2 = number;
+      while (num2/i > 0){
+        x+=Math.floor(num2/i);
+        num2 = Math.floor(num2/i);
+      }
+    if(num > x/a){ 
+      num=x/a;
+    } 
+
+  }
+return Math.floor(num);
 }
